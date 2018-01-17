c9bfc9d228865421fac3d6d1216db1bd60593500
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-01-17 20:25:45 +0100

    Remove examples

--- examples/border.rs
+++
@@ -1,9 +0,0 @@
-// Run with `cargo run --example border --features border`.
-
-extern crate chunkwm;
-
-use chunkwm::common::border::*;
-
-fn main() {
-    Border::new(10, 10, 50, 50, 5, 0, 0xFFFFFFFF);
-}
