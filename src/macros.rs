0aea47b4c005bd4ab5adfed734b1e949e1e683fa
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-01-25 18:24:19 +0100

    Use only one event send function.

--- src/macros.rs
+++ src/macros.rs
@@ -131,7 +131,7 @@ macro_rules! create_c_bridge {
     ($struct_ident:ident) => {pub mod _export {
         use chunkwm::prelude::*;
         use super::$struct_ident;
-        use std::os::raw::c_char;
+        use std::os::raw::{c_char, c_void};
         use std::ffi;
         use std::mem::transmute;
 
@@ -179,168 +179,85 @@ macro_rules! create_c_bridge {
             _handler.shutdown();
         }
 
-        // Send events containing an application to the event handler.
         #[no_mangle]
-        pub unsafe extern "C" fn chunkwm_rust_send_event_with_application(
-            ptr: *mut $struct_ident,
-            event: *const c_char,
-            application: ApplicationRef,
-        ) {
-            let _handler = &mut *ptr;
-            let application: Box<Application> = Box::new(application.into());
+        pub unsafe extern "C" fn chunkwm_rust_send_event(handler_ptr: *mut $struct_ident, event: *const c_char, data: *mut c_void) -> bool {
+            let _handler = &mut *handler_ptr;
             let event = ffi::CStr::from_ptr(event).to_string_lossy().into_owned();
 
-            match event.as_str() {
+            let handled: Result<(), ChunkWMError> = match event.as_str() {
                 "chunkwm_export_application_launched" => {
-                    _handler.handle(Event::ApplicationLaunched(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationLaunched(Box::new((data as ApplicationRef).into())))
                 }
                 "chunkwm_export_application_terminated" => {
-                    _handler.handle(Event::ApplicationTerminated(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationTerminated(Box::new((data as ApplicationRef).into())))
                 }
                 "chunkwm_export_application_activated" => {
-                    _handler.handle(Event::ApplicationActivated(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationActivated(Box::new((data as ApplicationRef).into())))
                 }
                 "chunkwm_export_application_deactivated" => {
-                    _handler.handle(Event::ApplicationDeactivated(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationDeactivated(Box::new((data as ApplicationRef).into())))
                 }
                 "chunkwm_export_application_hidden" => {
-                    _handler.handle(Event::ApplicationHidden(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationHidden(Box::new((data as ApplicationRef).into())))
                 }
                 "chunkwm_export_application_unhidden" => {
-                    _handler.handle(Event::ApplicationUnhidden(application))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::ApplicationUnhidden(Box::new((data as ApplicationRef).into())))
                 }
-                _ => {
-                    _handler.handle(Event::Other(event))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
-                }
-            };
-        }
-
-        // Send events containing a window to the event handler.
-        #[no_mangle]
-        pub unsafe extern "C" fn chunkwm_rust_send_event_with_window(
-            ptr: *mut $struct_ident,
-            event: *const c_char,
-            window: WindowRef,
-        ) {
-            let _handler = &mut *ptr;
-            let window: Box<Window> = Box::new(window.into());
-            let event = ffi::CStr::from_ptr(event).to_string_lossy().into_owned();
-
-            match event.as_str() {
                 "chunkwm_export_window_created" => {
-                    _handler.handle(Event::WindowCreated(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowCreated(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_destroyed" => {
-                    _handler.handle(Event::WindowDestroyed(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowDestroyed(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_focused" => {
-                    _handler.handle(Event::WindowFocused(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowFocused(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_moved" => {
-                    _handler.handle(Event::WindowMoved(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowMoved(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_resized" => {
-                    _handler.handle(Event::WindowResized(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowResized(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_minimized" => {
-                    _handler.handle(Event::WindowMinimized(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowMinimized(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_deminimized" => {
-                    _handler.handle(Event::WindowDeminimized(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowDeminimized(Box::new((data as WindowRef).into())))
                 }
                 "chunkwm_export_window_title_changed" => {
-                    _handler.handle(Event::WindowTitleChanged(window))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::WindowTitleChanged(Box::new((data as WindowRef).into())))
                 }
-                _ => {
-                    _handler.handle(Event::Other(event))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
-                }
-            };
-        }
-
-        // Send events containing a display to the event handler.
-        #[no_mangle]
-        pub unsafe extern "C" fn chunkwm_rust_send_event_with_display(
-            ptr: *mut $struct_ident,
-            event: *const c_char,
-            display: DisplayID,
-        ) {
-            let _handler = &mut *ptr;
-            let event = ffi::CStr::from_ptr(event).to_string_lossy().into_owned();
-
-            match event.as_str() {
                 "chunkwm_export_display_added" => {
-                    _handler.handle(Event::DisplayAdded(display))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::DisplayAdded(*(data as *mut DisplayID)))
                 }
                 "chunkwm_export_display_removed" => {
-                    _handler.handle(Event::DisplayRemoved(display))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::DisplayRemoved(*(data as *mut DisplayID)))
                 }
                 "chunkwm_export_display_moved" => {
-                    _handler.handle(Event::DisplayMoved(display))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::DisplayMoved(*(data as *mut DisplayID)))
                 }
                 "chunkwm_export_display_resized" => {
-                    _handler.handle(Event::DisplayResized(display))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                    _handler.handle(Event::DisplayResized(*(data as *mut DisplayID)))
                 }
-                _ => {
-                    _handler.handle(Event::Other(event))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
-                }
-            };
-        }
-
-        // Send events containing payloads to the event handler.
-        #[no_mangle]
-        pub unsafe extern "C" fn chunkwm_rust_send_event_with_daemon_command(
-            ptr: *mut $struct_ident,
-            payload: PayloadRef,
-        ) {
-            let _handler = &mut *ptr;
-            let payload: Payload = payload.into();
-
-            _handler.handle(Event::DaemonCommand(payload))
-                .unwrap_or_else(|e| eprintln!("{}", e));
-        }
-
-        // Send events containing 'nothing' or unknown events to the event handler.
-        #[no_mangle]
-        pub unsafe extern "C" fn chunkwm_rust_send_event_with_nothing(
-            ptr: *mut $struct_ident,
-            event: *const c_char,
-        ) {
-            let _handler = &mut *ptr;
-            let event = ffi::CStr::from_ptr(event).to_string_lossy().into_owned();
-
-            match event.as_str() {
                 "chunkwm_export_display_changed" => {
                     _handler.handle(Event::DisplayChanged)
-                        .unwrap_or_else(|e| eprintln!("{}", e))
                 }
                 "chunkwm_export_space_changed" => {
                     _handler.handle(Event::SpaceChanged)
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                }
+                "chunkwm_daemon_command" => {
+                    _handler.handle(Event::DaemonCommand((data as PayloadRef).into()))
                 }
                 _ => {
                     _handler.handle(Event::Other(event))
-                        .unwrap_or_else(|e| eprintln!("{}", e))
+                }
+            };
+
+            match handled {
+                Ok(_) => true,
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                    false
                 }
             }
         }
