a9d4889af7702a3e74a0da1f6ee46d914847617c
Travis <travis@Traviss-Mac-463.local>
2018-03-08 20:13:29 +0000

    Automatic Travis documentation build

--- master/main.js
+++ master/main.js
@@ -47,8 +47,6 @@
     // 2 for "In Return Types"
     var currentTab = 0;
 
-    var themesWidth = null;
-
     function hasClass(elem, className) {
         if (elem && className && elem.className) {
             var elemClass = elem.className;
@@ -123,10 +121,7 @@
                 sidebar.appendChild(div);
             }
         }
-        var themePicker = document.getElementsByClassName("theme-picker");
-        if (themePicker && themePicker.length > 0) {
-            themePicker[0].style.display = "none";
-        }
+        document.getElementsByTagName("body")[0].style.marginTop = '45px';
     }
 
     function hideSidebar() {
@@ -141,10 +136,6 @@
             filler.remove();
         }
         document.getElementsByTagName("body")[0].style.marginTop = '';
-        var themePicker = document.getElementsByClassName("theme-picker");
-        if (themePicker && themePicker.length > 0) {
-            themePicker[0].style.display = null;
-        }
     }
 
     // used for special search precedence
@@ -267,7 +258,6 @@
                 addClass(search, "hidden");
                 removeClass(document.getElementById("main"), "hidden");
             }
-            defocusSearchBar();
             break;
 
         case "s":
@@ -362,33 +352,35 @@
      * This code is an unmodified version of the code written by Marco de Wit
      * and was found at http://stackoverflow.com/a/18514751/745719
      */
-    var levenshtein_row2 = [];
-    function levenshtein(s1, s2) {
-        if (s1 === s2) {
-            return 0;
-        }
-        var s1_len = s1.length, s2_len = s2.length;
-        if (s1_len && s2_len) {
-            var i1 = 0, i2 = 0, a, b, c, c2, row = levenshtein_row2;
-            while (i1 < s1_len) {
-                row[i1] = ++i1;
+    var levenshtein = (function() {
+        var row2 = [];
+        return function(s1, s2) {
+            if (s1 === s2) {
+                return 0;
             }
-            while (i2 < s2_len) {
-                c2 = s2.charCodeAt(i2);
-                a = i2;
-                ++i2;
-                b = i2;
-                for (i1 = 0; i1 < s1_len; ++i1) {
-                    c = a + (s1.charCodeAt(i1) !== c2 ? 1 : 0);
-                    a = row[i1];
-                    b = b < a ? (b < c ? b + 1 : c) : (a < c ? a + 1 : c);
-                    row[i1] = b;
+            var s1_len = s1.length, s2_len = s2.length;
+            if (s1_len && s2_len) {
+                var i1 = 0, i2 = 0, a, b, c, c2, row = row2;
+                while (i1 < s1_len) {
+                    row[i1] = ++i1;
                 }
+                while (i2 < s2_len) {
+                    c2 = s2.charCodeAt(i2);
+                    a = i2;
+                    ++i2;
+                    b = i2;
+                    for (i1 = 0; i1 < s1_len; ++i1) {
+                        c = a + (s1.charCodeAt(i1) !== c2 ? 1 : 0);
+                        a = row[i1];
+                        b = b < a ? (b < c ? b + 1 : c) : (a < c ? a + 1 : c);
+                        row[i1] = b;
+                    }
+                }
+                return b;
             }
-            return b;
-        }
-        return s1_len + s2_len;
-    }
+            return s1_len + s2_len;
+        };
+    })();
 
     function initSearch(rawSearchIndex) {
         var currentResults, index, searchIndex;
@@ -407,20 +399,12 @@
         /**
          * Executes the query and builds an index of results
          * @param  {[Object]} query     [The user query]
+         * @param  {[type]} max         [The maximum results returned]
          * @param  {[type]} searchWords [The list of search words to query
          *                               against]
          * @return {[type]}             [A search index of results]
          */
-        function execQuery(query, searchWords) {
-            function itemTypeFromName(typename) {
-                for (var i = 0; i < itemTypes.length; ++i) {
-                    if (itemTypes[i] === typename) {
-                        return i;
-                    }
-                }
-                return -1;
-            }
-
+        function execQuery(query, max, searchWords) {
             var valLower = query.query.toLowerCase(),
                 val = valLower,
                 typeFilter = itemTypeFromName(query.type),
@@ -609,7 +593,7 @@
                 var lev_distance = MAX_LEV_DISTANCE + 1;
                 if (obj.name === val.name) {
                     if (literalSearch === true) {
-                        if (val.generics && val.generics.length !== 0) {
+                        if (val.generics.length !== 0) {
                             if (obj.generics && obj.length >= val.generics.length) {
                                 var elems = obj.generics.slice(0);
                                 var allFound = true;
@@ -646,7 +630,7 @@
                 }
                 // Names didn't match so let's check if one of the generic types could.
                 if (literalSearch === true) {
-                     if (obj.generics && obj.generics.length > 0) {
+                     if (obj.generics.length > 0) {
                         for (var x = 0; x < obj.generics.length; ++x) {
                             if (obj.generics[x] === val.name) {
                                 return true;
@@ -1036,8 +1020,9 @@
             return true;
         }
 
-        function getQuery(raw) {
-            var matches, type, query;
+        function getQuery() {
+            var matches, type, query, raw =
+                document.getElementsByClassName('search-input')[0].value;
             query = raw;
 
             matches = query.match(/^(fn|mod|struct|enum|trait|type|const|macro)\s*:\s*/i);
@@ -1146,10 +1131,6 @@
                     e.preventDefault();
                 } else if (e.which === 16) { // shift
                     // Does nothing, it's just to avoid losing "focus" on the highlighted element.
-                } else if (e.which === 27) { // escape
-                    removeClass(actives[currentTab][0], 'highlighted');
-                    document.getElementsByClassName('search-input')[0].value = '';
-                    defocusSearchBar();
                 } else if (actives[currentTab].length > 0) {
                     removeClass(actives[currentTab][0], 'highlighted');
                 }
@@ -1241,7 +1222,7 @@
         }
 
         function showResults(results) {
-            var output, query = getQuery(document.getElementsByClassName('search-input')[0].value);
+            var output, query = getQuery();
 
             currentResults = query.id;
             output = '<h1>Results for ' + escape(query.query) +
@@ -1285,7 +1266,7 @@
                 resultIndex;
             var params = getQueryStringParams();
 
-            query = getQuery(document.getElementsByClassName('search-input')[0].value);
+            query = getQuery();
             if (e) {
                 e.preventDefault();
             }
@@ -1307,8 +1288,17 @@
                 }
             }
 
-            results = execQuery(query, index);
+            results = execQuery(query, 20000, index);
             showResults(results);
+        }
+
+        function itemTypeFromName(typename) {
+            for (var i = 0; i < itemTypes.length; ++i) {
+                if (itemTypes[i] === typename) {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         function buildIndex(rawSearchIndex) {
@@ -1541,9 +1531,7 @@
                 ul.appendChild(li);
             }
             div.appendChild(ul);
-            if (sidebar) {
-                sidebar.appendChild(div);
-            }
+            sidebar.appendChild(div);
         }
 
         block("primitive", "Primitive Types");
@@ -1895,9 +1883,4 @@
 // Sets the focus on the search bar at the top of the page
 function focusSearchBar() {
     document.getElementsByClassName('search-input')[0].focus();
-}
-
-// Removes the focus from the search bar
-function defocusSearchBar() {
-    document.getElementsByClassName('search-input')[0].blur();
 }
