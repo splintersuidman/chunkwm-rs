dc2ad43db178c9e76e00422a3ff7bac465a6c052
Travis <travis@Traviss-Mac-463.local>
2018-05-09 06:09:23 +0000

    Automatic Travis documentation build

--- master/implementors/core/ops/bit/trait.BitOr.js
+++ master/implementors/core/ops/bit/trait.BitOr.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["bitflags"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"bitflags/example_generated/struct.Flags.html\" title=\"struct bitflags::example_generated::Flags\">Flags</a>",synthetic:false,types:["bitflags::example_generated::Flags"]},];
 implementors["core_graphics"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"core_graphics/event/struct.CGEventFlags.html\" title=\"struct core_graphics::event::CGEventFlags\">CGEventFlags</a>",synthetic:false,types:["core_graphics::event::CGEventFlags"]},];
 
             if (window.register_implementors) {
