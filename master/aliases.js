2976ad8167e787dafe197dd1e92715ea3a47ecaf
Travis <travis@Traviss-Mac-463.local>
2018-05-20 11:07:52 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -1,9 +1,17 @@
 var ALIASES = {};
 ALIASES['bitflags'] = {};
+ALIASES['bitflags'] = {};
+ALIASES['chunkwm'] = {};
 ALIASES['chunkwm'] = {};
 ALIASES['core_foundation'] = {};
+ALIASES['core_foundation'] = {};
+ALIASES['core_foundation_sys'] = {};
 ALIASES['core_foundation_sys'] = {};
 ALIASES['core_graphics'] = {};
+ALIASES['core_graphics'] = {};
+ALIASES['foreign_types'] = {};
 ALIASES['foreign_types'] = {};
 ALIASES['foreign_types_shared'] = {};
+ALIASES['foreign_types_shared'] = {};
+ALIASES['libc'] = {};
 ALIASES['libc'] = {};
